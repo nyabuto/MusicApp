9544044d678cd76eae8f27783dd275d980307f49
Geofrey Nyabuto <mwambage@gmail.com>
2016-08-12 09:14:12 +0300

    Initial Commit

---
+++ MusicApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MusicApp
+//
+//  Created by ilabadmin on 8/12/16.
+//  Copyright © 2016 softikoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MusicApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MusicApp
+//
+//  Created by ilabadmin on 8/12/16.
+//  Copyright © 2016 softikoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
